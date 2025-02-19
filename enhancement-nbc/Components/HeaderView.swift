73f79215f652f7435ec6de26b0e00a06f52628f1
QuaRang <dbduddnd1225@gmail.com>
2025-02-19 15:42:56 +0900

    #8 - HeaderView 구현
    - View 재사용성을 용이하게 하기 위해 커스텀 HeaderView 구현
    - rightItem, leftItem, title이 존재하며 HeaderView가 필요한 곳에서 각각의 아이템 속성 재정의할 수 있음
    - 크기 및 레이아웃 설정

--- enhancement-nbc/Components/HeaderView.swift
+++ enhancement-nbc/Components/HeaderView.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import UIKit
+
+class HeaderView:UIView{
+    let rightItem:UIButton={
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    let title:UILabel={
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let leftItem:UIButton={
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    init() {
+        let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 100)
+        super.init(frame: frame)
+        addSubview(rightItem)
+        addSubview(title)
+        addSubview(leftItem)
+        backgroundColor = .white
+        configureLayout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func configureLayout(){
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            title.centerXAnchor.constraint(equalTo: centerXAnchor),
+            title.centerYAnchor.constraint(equalTo: bottomAnchor,constant: -20),
+            
+            rightItem.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            rightItem.centerYAnchor.constraint(equalTo: bottomAnchor,constant: -20),
+            
+            leftItem.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            leftItem.centerYAnchor.constraint(equalTo: bottomAnchor,constant: -20)
+        ])
+    }
+}
