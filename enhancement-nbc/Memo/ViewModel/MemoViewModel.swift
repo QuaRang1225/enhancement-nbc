c5b7541a87c4231fd5cb53b6f36ce85f11df7e06
QuaRang <dbduddnd1225@gmail.com>
2025-02-19 15:46:40 +0900

    #8 - ViewModel 구현
    - 메모기능의 비즈니스 로직을 모아둔 클래스
    - 데이터 저장, 삭제 ,불러오기, 업데이트 등의 기능 구현
    - 기능 실행 후 ViewController에서 추가적으로 필요한 기능을 파라미터로 받아 사용하도록 구현

--- enhancement-nbc/Memo/ViewModel/MemoViewModel.swift
+++ enhancement-nbc/Memo/ViewModel/MemoViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+class MemoViewModel {
+    var memoList: [Memo] = []
+    private let defaults = UserDefaults.standard
+    
+    var onUpdate: (() -> Void)?
+    
+    init() {
+        loadMemoList()
+    }
+    //데이터 불러오기
+    func loadMemoList() {
+        guard let data = defaults.object(forKey: "memo") as? Data,
+              let loadedMemo = try? JSONDecoder().decode([Memo].self, from: data) else { return }
+        memoList = loadedMemo
+        onUpdate?()
+    }
+    //데이터 저장
+    func saveMemo(_ memo:Memo){
+        memoList.append(memo)
+        updateDefaults()
+        onUpdate?()
+    }
+    //데이터 삭제
+    func deleteMemo(_ indexPath:IndexPath){
+        memoList.remove(at: indexPath.row)
+        updateDefaults()
+        onUpdate?()
+    }
+    //데이터 상태 업데이트
+    private func updateDefaults(){
+        let encoder = JSONEncoder()
+        guard let encoded = try? encoder.encode(memoList) else { return }
+        defaults.set(encoded, forKey: "memo")
+    }
+    
+}
