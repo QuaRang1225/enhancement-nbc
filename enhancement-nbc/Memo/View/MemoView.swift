3f8ff34f2801830e0d7336c22d45e32b41c18df3
QuaRang <dbduddnd1225@gmail.com>
2025-02-19 15:44:34 +0900

    #8 - MemoView 구현
    - 매모앱의 실제 컴포넌트들을 배치한 클래스
    - tableView와 headerView 정의 및 레이아웃 설정

--- enhancement-nbc/Memo/View/MemoView.swift
+++ enhancement-nbc/Memo/View/MemoView.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUI
+
+class MemoView:UIView{
+    
+    let headerView = HeaderView()
+    let tableView = UITableView(frame: .zero,style: .insetGrouped)
+    
+    func configureView(){
+        addSubview(tableView)
+        addSubview(headerView)
+        configureHeaderView()
+    }
+    func configureHeaderView(){
+        headerView.title.text = "Memo"
+        headerView.rightItem.setImage(UIImage(systemName: "plus"), for: .normal)
+        headerView.leftItem.setImage(UIImage(systemName: "exclamationmark.circle.fill"), for: .normal)
+        headerView.leftItem.tintColor = .red
+    }
+    func configureLayout(){
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            
+            headerView.topAnchor.constraint(equalTo: topAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 100),
+            headerView.leftAnchor.constraint(equalTo: leftAnchor),
+            headerView.rightAnchor.constraint(equalTo: rightAnchor)
+        ])
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+#Preview {
+    UIViewPreview(MemoView()).ignoresSafeArea()
+}
+
