92b01d27863fd2d15b37474fda81ae9c15b35dc9
QuaRang <dbduddnd1225@gmail.com>
2025-03-24 16:55:25 +0900

    #16 - UI요소 구현
    - 타이틀 및 시리즈 순서 UI 구현
    - view에 추가 및 오토 레이아웃 설정

--- project-book-series/project-book-series/Book/BookView.swift
+++ project-book-series/project-book-series/Book/BookView.swift
@@ -7,16 +7,54 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 //MARK: BookView
 //UI요소 분리
 final class BookView:UIView{
     
+    //MARK: 타이틀
+    private let titleLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Harry Potter and the Philosopher’s Stone "
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 24)
+        return label
+    }()
+    //MARK: 시리즈 순서
+    private let seriesButton:UIButton = {
+        let button = UIButton()
+        button.setTitle("1", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 20
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureUI()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: 컴포넌트 및 레이아웃 설정
+    private func configureUI(){
+        [titleLabel,seriesButton]
+            .forEach{ addSubview($0) }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(10)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(20)
+        }
+        seriesButton.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(16)
+            make.width.height.equalTo(40)
+            make.centerX.equalToSuperview()
+        }
     }
 }
 
