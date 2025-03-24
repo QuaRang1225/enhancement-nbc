b1606e86b0387dc7b4e65a2a05802b097c3db43c
QuaRang <dbduddnd1225@gmail.com>
2025-03-24 13:18:24 +0900

    #16 - 프로젝트 생성

---
+++ project-book-series/project-book-series/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-book-series
+//
+//  Created by 유영웅 on 3/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ project-book-series/project-book-seriesTests/project_book_seriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  project_book_seriesTests.swift
+//  project-book-seriesTests
+//
+//  Created by 유영웅 on 3/24/25.
+//
+
+import Testing
+@testable import project_book_series
+
+struct project_book_seriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
