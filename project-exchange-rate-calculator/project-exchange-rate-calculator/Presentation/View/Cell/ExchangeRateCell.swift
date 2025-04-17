eaaa748150f6a759c9e3a1eaba1d801b4c0928e5
QuaRang <dbduddnd1225@gmail.com>
2025-04-17 12:39:51 +0900

    feat: 즐겨찾기 버튼 Cell에 추가 (#31)

--- project-exchange-rate-calculator/project-exchange-rate-calculator/Presentation/View/Cell/ExchangeRateCell.swift
+++ project-exchange-rate-calculator/project-exchange-rate-calculator/Presentation/View/Cell/ExchangeRateCell.swift
@@ -33,6 +33,12 @@ final class ExchangeRateCell: UITableViewCell {
         $0.font = .systemFont(ofSize: 14)
     }
     
+    // 즐겨찾기 라벨
+    public let bookmarkButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "star"), for: .normal)
+        $0.tintColor = .systemYellow
+    }
+    
     // 셀 컨텐츠 뷰
     private lazy var labelStackView = UIStackView(arrangedSubviews: [currencyLabel, countryLabel]).then {
         $0.axis = .vertical
@@ -58,23 +64,31 @@ final class ExchangeRateCell: UITableViewCell {
     
     // sub view 추가
     private func configureSubView(){
-        [rateLabel, labelStackView]
+        [rateLabel, labelStackView, bookmarkButton]
             .forEach{ addSubview($0) }
     }
     
     // 오토 레이아웃
     private func configureLayout(){
+        
         // 라벨 스택 뷰
         labelStackView.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(16)
             $0.centerY.equalToSuperview()
         }
+        
         // 환율 라벨
         rateLabel.snp.makeConstraints {
-            $0.trailing.equalToSuperview().offset(-16)
             $0.centerY.equalToSuperview()
             $0.leading.greaterThanOrEqualTo(labelStackView.snp.trailing).offset(16)
             $0.width.equalTo(120)
+        }
+        
+        //즐겨 찾기 버튼
+        bookmarkButton.snp.makeConstraints {
+            $0.leading.equalTo(rateLabel.snp.trailing).offset(16)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-16)
         }
     }
 }
