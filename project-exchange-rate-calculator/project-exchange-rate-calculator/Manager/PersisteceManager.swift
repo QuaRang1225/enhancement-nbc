a758e7dcfef6406f9088016c6ec1e2e9fb845016
QuaRang <dbduddnd1225@gmail.com>
2025-04-17 15:30:02 +0900

    feat: PersistenceManager 구현 (#31)
    - container 및 context 제공

---
+++ project-exchange-rate-calculator/project-exchange-rate-calculator/Manager/PersisteceManager.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataStack.swift
+//  project-exchange-rate-calculator
+//
+//  Created by 유영웅 on 4/17/25.
+//
+
+import Foundation
+import CoreData
+
+// MARK: NSPersistentContainer 설정과 viewContext, backgroundContext 제공
+final class PersisteceManager {
+    
+    static let shared = PersisteceManager()
+    
+    // Core Data의 전체 스택을 관리하는 컨테이너
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("컨테이너 로딩 실패: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    // Core Data의 작업 공간(CRUD)
+    var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+}
