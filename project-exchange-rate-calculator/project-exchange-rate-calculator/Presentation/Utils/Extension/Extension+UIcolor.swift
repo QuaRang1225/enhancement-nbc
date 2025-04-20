636e50f60ec60745df7627ba2d6f2d329649caff
QuaRang <dbduddnd1225@gmail.com>
2025-04-20 15:51:52 +0900

    feat: 다크모드용 커스텀 색상 추가 (#33)
    - 각 색상 별로 메서드에서 사용하기 위해 extension으로 재정의

---
+++ project-exchange-rate-calculator/project-exchange-rate-calculator/Presentation/Utils/Extension/Extension+UIcolor.swift
@@ -0,0 +1,19 @@
+//
+//  Extension+UIcolor.swift
+//  project-exchange-rate-calculator
+//
+//  Created by 유영웅 on 4/20/25.
+//
+
+import Foundation
+import UIKit
+
+// 커스텀 색상 적용
+extension UIColor {
+    static let customBackgroundColor = UIColor(named: "customBackgroundColor")
+    static let customTextColor = UIColor(named: "customTextColor")
+    static let customSecondaryTextColor = UIColor(named: "customSecondaryTextColor")
+    static let customCellBackgroundColor = UIColor(named: "customCellBackgroundColor")
+    static let customFavoriteColor = UIColor(named: "customFavoriteColor")
+    static let customButtonColor = UIColor(named: "customButtonColor")
+}
