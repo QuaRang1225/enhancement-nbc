f2114982da4aff68cd3c2c336e0b3dfec804a0b9
QuaRang <dbduddnd1225@gmail.com>
2025-04-21 22:19:39 +0900

    feat: DIContainer 구현 (#36)
    - Repository, UseCase, ViewModel, ViewController를 각각 객체 생성 후 연결

---
+++ project-exchange-rate-calculator/project-exchange-rate-calculator/App/DIContainer/ExchangeRateDIContainer.swift
@@ -0,0 +1,31 @@
+//
+//  DIContainer.swift
+//  project-exchange-rate-calculator
+//
+//  Created by 유영웅 on 4/21/25.
+//
+
+import Foundation
+
+final class ExchangeRateDIContainer {
+
+    // MARK: - Repository
+    private var makeExchangeRateRepository: ExchangeRateRepository {
+        DefaultExchangeRateRepository(api: makeNetworkService)
+    }
+
+    // MARK: - UseCase
+    private var makeExchangeRateUseCase: FetchExchangeRateUseCase {
+        DefaultFetchExchangeRateUseCase(repository: makeExchangeRateRepository)
+    }
+
+    // MARK: - ViewModel
+    private var makeMainViewModel: MainViewModel {
+        MainViewModel(useCase: makeExchangeRateUseCase)
+    }
+
+    // MARK: - ViewController
+    private var makeMainViewController: MainViewController {
+        MainViewController(viewModel: makeMainViewModel)
+    }
+}
